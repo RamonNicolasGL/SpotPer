59e5f5d80f5fab40b7901cee2c679afab65d7429
LucasSLopes <lucas470sousa@gmail.com>
2023-11-18 17:06:06 -0300

    add entidade composição

---
+++ atributos_faixa.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Composicao (
+	id_composicao INT IDENTITY(1, 1) NOT NULL, -- gera o codigo de forma incremental
+	descricao VARCHAR(100)NOT NULL,
+
+	CONSTRAINT composicao_PK PRIMARY KEY (id_composicao),
+	CONSTRAINT descricao_UNIQUE UNIQUE (descricao)
+)
