4002e259cd70cba64aa616c15abab23dc8fac987
LucasSLopes <lucas470sousa@gmail.com>
2023-11-18 17:06:49 -0300

    add entidade Periodo e Compositor

--- atributos_faixa.sql
+++ atributos_faixa.sql
@@ -5,3 +5,22 @@ CREATE TABLE Composicao (
 	CONSTRAINT composicao_PK PRIMARY KEY (id_composicao),
 	CONSTRAINT descricao_UNIQUE UNIQUE (descricao)
 )
+
+
+CREATE TABLE Periodo_musical (
+	id_periodo smallint IDENTITY(1, 1) not null,
+	descricao varchar(50) not null,
+	tempo_ativ varchar(50) not null,
+
+	CONSTRAINT periodo_musical_PK PRIMARY KEY (id_periodo),
+	CONSTRAINT descricao_UNIQUE UNIQUE (descricao)
+)
+
+
+CREATE TABLE Compositor (
+	id_compositor smallint IDENTITY(1, 1) not null,
+	nome varchar(50) not null,
+	data_nasc DATE not null,
+	data_morte DATE,
+	local_nasc varchar(50) not null,
+	id_periodo smallint not null,
