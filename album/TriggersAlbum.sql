51a489d8f48e1c6c0a91f8bb2637cddbf8897747
Ramon Nicolas <100383915+RamonNicolasGL@users.noreply.github.com>
2023-11-24 21:15:16 -0300

    Update TriggersAlbum.sql

--- album/TriggersAlbum.sql
+++ album/TriggersAlbum.sql
@@ -1,7 +1,7 @@
 -- Ele Buga quando vc adiciona um album que tenha uma faixa DDD e outras diferentes de DDD, considera esse album pro calculo da média
 -- Ou seja, nao pega SOMENTE os albuns com todas as musicas DDD
 --Validar preeço da compra > 3*media dos albuns ocm todas as faixas DDD
-CREATE TRIGGER valdiar_preco_compra_album
+CREATE TRIGGER validar_preco_compra_album
 ON Album
 AFTER INSERT, UPDATE
 AS
@@ -10,8 +10,12 @@ BEGIN
 
     SELECT @MediaPrecoCompras = AVG(a.preco_compra)
     FROM Album a
-    JOIN Faixas f ON a.id_album = f.id_album_fk
-    WHERE f.tipo_gravacao = 'DDD';
+    WHERE (
+        SELECT COUNT(*)
+        FROM Faixas f
+        WHERE f.id_album_fk = a.id_album
+          AND f.tipo_gravacao <> 'DDD'
+    ) = 0
 
     IF EXISTS (
         SELECT 1
@@ -19,7 +23,7 @@ BEGIN
         WHERE i.preco_compra > 3 * @MediaPrecoCompras
     )
     BEGIN
-        RAISERROR('O preço de compra não pode ser superior a três vezes a média dos albuns com todas as Faixas tipo DDD.', 16, 1);
+        RAISEERROR('O preço de compra não pode ser superior a três vezes a média dos álbuns com todas as faixas tipo DDD.', 16, 1);
         ROLLBACK TRANSACTION;
     END
 END;
