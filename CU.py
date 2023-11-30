b7c53c798fee83dd9baeee6733ffaa036931c150
Ramon Nicolas <100383915+RamonNicolasGL@users.noreply.github.com>
2023-11-30 20:39:54 -0300

    Exmp de funções

--- CU.py
+++ CU.py
@@ -18,3 +18,40 @@ resultado = cursor.execute('SELECT * FROM Faixas').fetchall()
 
 for linha in resultado:
     print(linha)
+
+
+############# TESTAR AINDA
+def listar_albums(cursor):
+    query = 'SELECT AlbumId, NomeAlbum FROM Albuns'
+    cursor.execute(query)
+    albums = cursor.fetchall()
+    for album in albums:
+        print(f"{album.AlbumId}: {album.NomeAlbum}")
+    return albums
+
+def listar_faixas_do_album(cursor, album_id):
+    query = f'SELECT * FROM Faixas WHERE AlbumId = {album_id}'
+    cursor.execute(query)
+    faixas = cursor.fetchall()
+    for faixa in faixas:
+        print(f"{faixa.FaixaId}: {faixa.NomeFaixa}")
+    return faixas
+
+conexao, cursor = conectar_ao_banco()
+
+# Listar todos os álbuns
+albums = listar_albums(cursor)
+
+# Permitir a seleção de um álbum
+while True:
+    try:
+        album_id_selecionado = int(input("Selecione o ID do álbum desejado: "))
+        break
+    except ValueError:
+        print("Por favor, insira um número válido.")
+
+# Listar todas as faixas do álbum selecionado
+listar_faixas_do_album(cursor, album_id_selecionado)
+
+# Fechar a conexão com o banco de dados
+conexao.close()
