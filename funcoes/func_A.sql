bd2987f4917112ae3c07382edc11d4b3c32d4872
LucasSLopes <lucas470sousa@gmail.com>
2023-12-02 11:34:43 -0300

    funções A, B, C e D adicionadas

--- funcoes/func_A.sql
+++ funcoes/func_A.sql
@@ -39,3 +39,46 @@ RETURN (
 		order by "countPlayslit" desc
 )
 
+
+CREATE FUNCTION faixas_barroco_concerto()
+RETURNS TABLE
+AS
+RETURN (
+select p.id_playlist, p.nome from 
+	Album a join
+	Faixas f on f.id_album_fk = id_album join
+	Faixa_Compositor fc on fc.id_faixa_fk = f.id_faixa join
+	Compositor c on c.id_compositor = fc.id_compositor_fk join
+	Periodo_musical pm on pm.id_periodo = c.id_periodo_fk join 
+	Composicao cp on cp.id_composicao = f.id_composicao_fk join 
+	Playlist_Faixas pf on pf.faixa = f.id_faixa join
+	Playlist p on p.id_playlist = pf. playlist
+
+	where cp.descricao = 'Concerto' and pm.descricao = 'Barroco'  and 
+		NOT EXISTS (SELECT 1 
+    FROM Album a 
+    JOIN Faixas f ON f.id_album_fk = id_album 
+    JOIN Faixa_Compositor fc ON fc.id_faixa_fk = f.id_faixa 
+    JOIN Compositor c ON c.id_compositor = fc.id_compositor_fk 
+    JOIN Periodo_musical pm ON pm.id_periodo = c.id_periodo_fk 
+    JOIN Composicao cp ON cp.id_composicao = f.id_composicao_fk 
+    JOIN Playlist_Faixas pf ON pf.faixa = f.id_faixa 
+    WHERE pf.playlist = p.id_playlist AND (cp.descricao != 'Concerto' OR pm.descricao != 'Barroco'))
+	group by p.id_playlist, p.nome
+)
+
+CREATE FUNCTION compositor_mais_faixas_playlist()
+RETURNS TABLE
+AS
+RETURN (
+select TOP 1 c.id_compositor, c.nome, count(pf.faixa) as "num_faixas" from 
+	Album a join
+	Faixas f on f.id_album_fk = id_album join
+	Faixa_Compositor fc on fc.id_faixa_fk = f.id_faixa join
+	Compositor c on c.id_compositor = fc.id_compositor_fk join
+	Playlist_Faixas pf on pf.faixa = f.id_faixa join
+	Playlist p on p.id_playlist = pf.playlist
+
+	group by c.id_compositor, c.nome
+	order by "num_faixas" desc
+)
