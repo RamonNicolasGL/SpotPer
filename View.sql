8f1def7ac98e21e406541efc4e7119333501b053
Ramon Nicolas <100383915+RamonNicolasGL@users.noreply.github.com>
2023-12-03 00:23:50 -0300

    Update View.sql

--- View.sql
+++ View.sql
@@ -1,10 +1,11 @@
-create view VPlaylist_Qtd_Album (nome_playlist, qtd_albuns)
+create view Playlist_Qtd_Album (nome_playlist, qtd_albuns)
+with schemabinding
 as
 select p.nome as 'Nome da Playlist', count(DISTINCT a.id_album) as 'Quantidade de Albuns'
-FROM Playlist p
-LEFT JOIN Playlist_Faixas pf ON p.id_playlist = pf.playlist
-LEFT JOIN Faixas f ON pf.faixa = f.id_faixa
-LEFT JOIN Album a ON f.id_album_fk = a.id_album
+FROM dbo.Playlist p
+left JOIN dbo.Playlist_Faixas pf ON p.id_playlist = pf.playlist
+left JOIN dbo.Faixas f ON pf.faixa = f.id_faixa
+left JOIN dbo.Album a ON f.id_album_fk = a.id_album
 GROUP BY p.id_playlist, p.nome;
 
 --left join p pegar as playlists sem albuns
